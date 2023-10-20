f9a26bbc2994a7d34ec31baae53de1fdcb4b9e06
suman-rippey <suman@rippey.ai>
2023-10-20 19:32:10 +0545

    Load Receiver Email from Environment File

--- utils/send_email.py
+++ utils/send_email.py
@@ -9,7 +9,7 @@ def send_email(json_data):
     display_name = os.getenv("DISPLAY_NAME") # Load Display Name from environment file
     my_email=os.getenv("EMAIL") # Load Email Address from environment file
     my_password = os.getenv("PASSWORD") # Load Password from environment file
-    bot_email = '' # Set bot's email here
+    receiver_email = os.getenv("RECEIVER_EMAIL") # Load bot's email from environment file
 
     print(f"Sending as: {my_email}")
 
@@ -23,14 +23,14 @@ def send_email(json_data):
         msg = email.message.EmailMessage()
         print(f"Sending Data of {json['file_name']}")
         msg['From'] = f'{display_name} <{my_email}>'
-        msg['To'] = bot_email
+        msg['To'] = receiver_email
         msg['Subject'] = json['subject']
 
         # Encode the plain text content as UTF-8 and set charset
         msg.set_content(json['email_body'], subtype='plain', charset='utf-8')
 
         # Send the email
-        server.sendmail(my_email, bot_email, msg.as_string())
+        server.sendmail(my_email, receiver_email, msg.as_string())
         
     # Close the connection to the SMTP server
     server.quit()
